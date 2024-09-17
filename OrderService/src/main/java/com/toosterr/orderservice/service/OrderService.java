60d9d8e8fe71c7f41395efc2d6be1aa8def2d9fa
talha.rizwan <talha.rizwan7170@hotmail.com>
2024-09-17 17:35:12 +0500

    fixed issues related to copmpilation

--- OrderService/src/main/java/com/toosterr/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/toosterr/orderservice/service/OrderService.java
@@ -4,20 +4,13 @@ import com.toosterr.orderservice.dto.OrderRequest;
 import com.toosterr.orderservice.model.Order;
 import com.toosterr.orderservice.repository.OrderRepository;
 import com.toosterr.orderservice.util.JwtUtil;
-import com.toosterr.orderservice.model.Order;
-import com.toosterr.orderservice.repository.OrderRepository;
-import com.toosterr.orderservice.util.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
 
 @Service
 public class OrderService {
@@ -36,7 +29,6 @@ public class OrderService {
 
     public ResponseEntity<?> createOrder(OrderRequest orderRequest) {
         String url = "http://PRODUCT-SERVICE/api/v1/product/purchase/sku/{sku}";
-        String newToken = jwtUtil.createToken();
         String newToken = jwtUtil.createToken();
 
         try {
@@ -58,7 +50,6 @@ public class OrderService {
                         .build();
                 orderRepository.save(order);
                 kafkaTemplate.send("notificationTopic", "order created");
-                //Add kafka message
             }
 
             return new ResponseEntity<>(data, HttpStatus.OK);
